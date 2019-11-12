46d8087301f83ae11e0742c02cc42e4d52eee1d5
Jose Julian Ariza V. - IMPACTO TECNOLOGICO <josejulian@impactotecnologico.net>
2019-11-12 09:45:09 +0100

    inclusion de redux. Cambio para uso de mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import React from "react";
 import "./App.css";
 import "bulma/css/bulma.min.css";
 
-function App(props) {
-  const { expensesItems, addExpense } = props;
+// Importamos el método connect de React Redux
+import { connect } from "react-redux";
+/*
+ Connect nos permite poder conectar cualquier componente con nuestra Store, 
+ y así poder recibir el State y poder ejecutar acciones
+*/
 
+function App(props) {
+  // Iteracion 2
+  // Cambiaremos las funciones definidas
+  //const { expensesItems, addExpense } = props;
+  const { expensesItems, handleAddExpense } = props;
+  // Tenemos dos props: expensesItems que viene de la Store y handleAddExpense
+  // que es el action creator para modificar nuestro State
+
+  // Iteracion 2
+  // no eliminamos la funcion, la hemos reubicado de posición.
+  // Ahora la hemos definido dentro de la función mapDispatchToProps.
+  /*
   const handleAddExpense = () => {
     const newExpense = {
       id: 4,
@@ -13,6 +29,7 @@ function App(props) {
     };
     addExpense(newExpense);
   };
+*/
 
   return (
     <div className="container">
@@ -45,4 +62,30 @@ function App(props) {
   );
 }
 
-export default App;
+// cambiamos el export
+//export default App;
+
+const mapStateToProps = state => state;
+
+// Iteracion 1
+//export default connect(mapStateToProps)(App);
+
+// Iteracion 2
+// creamos una nueva función la cual será el segundo parámetro
+// que recibe la función connect de react-redux conocida como mapDispatchToProps
+// Devuelve el nuevo prop 'handleAddExpense'
+const mapDispatchToProps = dispatch => {
+  const newExpense = {
+    id: 4,
+    amount: 12.5,
+    description: "Coca-cola"
+  };
+  return {
+    handleAddExpense: () =>
+      dispatch({ type: "ADD_EXPENSE", payload: newExpense })
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps // Inyectamos las acciones a nuestro App.js
+)(App);
